184056531b04fbfd0d3a19d7ed0762ac6355ae78
Solomkin Alexandr <solomin1995@mail.ru>
2022-11-27 22:01:13 +0300

    create method that will process a string array and outputs the number of elements in the array that meet the requirements

--- FinalWork.cs
+++ FinalWork.cs
@@ -2,3 +2,16 @@
 
 string[] array = {"22", "3f24", "i4", "f321"," w", "5673g", "4 i", "1", " "};
 int maxLengthElementArray = 3;
+
+// 2. Разработка метода, который будет обрабатывать строковый массив и выдает количество элементов в массиве, удовлетворяющие требованиям
+
+int CounterNecessaryElementsArray(string[] array, int maxLengthElement){
+    int count = 0;
+    for(int i = 0; i < array.Length; i++){
+        string testElementArray = array[i];
+        if(testElementArray.Length <= maxLengthElement){
+            count++;
+        }
+    }
+    return count;
+}
